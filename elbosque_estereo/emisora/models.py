b2e7bcc0cd686edcc8991f91fe60c7425d2ea870
Cizaquita <cristian.izaquita@gmail.com>
2018-05-06 23:32:28 -0500

    Modificados campos de los modelos null+True en los que no son editables

--- elbosque_estereo/emisora/models.py
+++ elbosque_estereo/emisora/models.py
@@ -43,10 +43,10 @@ class User(models.Model):
 	usr_celphone = models.CharField(max_length=200)
 	usr_password = models.CharField(max_length=200)
 	# No editable no visible
-	usr_created_by = models.CharField(max_length=200, editable=False)
-	usr_modified_by = models.CharField(max_length=200, editable=False)
-	usr_created_date = models.DateTimeField('Creado', editable=False)
-	usr_modified_date = models.DateTimeField('Editado', editable=False)
+	usr_created_by = models.CharField(max_length=200, editable=False, null=True)
+	usr_modified_by = models.CharField(max_length=200, editable=False, null=True)
+	usr_created_date = models.DateTimeField('Creado', editable=False, null=True)
+	usr_modified_date = models.DateTimeField('Editado', editable=False, null=True)
 	# rol_id guarda al llave foránea de la clase rol
 	rol_id = models.ForeignKey(Role, on_delete=models.CASCADE)
 	usr_enabled = models.BooleanField(default=True)
@@ -61,10 +61,10 @@ class Category(models.Model):
 	ctg_description = models.CharField(max_length=100)
 	ctg_image_url = models.CharField(max_length=250)
 	# No editable no visible
-	ctg_modified_date = models.DateTimeField('Editado', editable=False)
-	ctg_created_date = models.DateTimeField('Creado', editable=False)
-	ctg_created_by = models.CharField(max_length=200, editable=False)
-	ctg_modified_by = models.CharField(max_length=200, editable=False)
+	ctg_modified_date = models.DateTimeField('Editado', editable=False, null=True)
+	ctg_created_date = models.DateTimeField('Creado', editable=False, null=True)
+	ctg_created_by = models.CharField(max_length=200, editable=False, null=True)
+	ctg_modified_by = models.CharField(max_length=200, editable=False, null=True)
 	ctg_enabled = models.BooleanField(default=True)
 	"""docstring for Category"""
 	def __str__(self):
@@ -77,10 +77,10 @@ class Subcategory(models.Model):
 	sct_description = models.CharField(max_length=100)
 	sct_image_url = models.CharField(max_length=250)
 	# No editable no visible
-	sct_modified_date = models.DateTimeField('Editado', editable=False)
-	sct_created_date = models.DateTimeField('Creado', editable=False)
-	sct_created_by = models.CharField(max_length=200, editable=False)
-	sct_modified_by = models.CharField(max_length=200, editable=False)
+	sct_modified_date = models.DateTimeField('Editado', editable=False, null=True)
+	sct_created_date = models.DateTimeField('Creado', editable=False, null=True)
+	sct_created_by = models.CharField(max_length=200, editable=False, null=True)
+	sct_modified_by = models.CharField(max_length=200, editable=False, null=True)
 	sct_enabled = models.BooleanField(default=True)
 	"""docstring for Category"""
 	def __str__(self):
@@ -95,10 +95,10 @@ class Program(models.Model):
 	prg_audio_url = models.CharField(max_length=250)
 	prg_image_url = models.CharField(max_length=250)
 	# No editable no visible
-	prg_modified_date = models.DateTimeField('Editado', editable=False)
-	prg_created_date = models.DateTimeField('Creado', editable=False)
-	prg_created_by = models.CharField(max_length=200, editable=False)
-	prg_modified_by = models.CharField(max_length=200, editable=False)
+	prg_modified_date = models.DateTimeField('Editado', editable=False, null=True)
+	prg_created_date = models.DateTimeField('Creado', editable=False, null=True)
+	prg_created_by = models.CharField(max_length=200, editable=False, null=True)
+	prg_modified_by = models.CharField(max_length=200, editable=False, null=True)
 	prg_enabled = models.BooleanField(default=True)
 	"""docstring for Category"""
 	def __str__(self):
