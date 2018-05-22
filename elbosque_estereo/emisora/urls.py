13c001862c3ee7f7518b8ab2f9b2a41f39a47366
Cizaquita <cristian.izaquita@gmail.com>
2018-05-22 01:04:11 -0500

    arreglada ruta de media/

--- elbosque_estereo/emisora/urls.py
+++ elbosque_estereo/emisora/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+ 
 from . import views
 
 urlpatterns = [
@@ -8,4 +11,4 @@ urlpatterns = [
 	path('parrilla/', views.parrilla, name='parrilla'),
 	path('get_parrilla/', views.get_parrilla, name='get_parrilla'),
 	path('play/', views.play, name='play'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
