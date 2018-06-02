dff6cec9a9ae3654348acce6436a3266a907c353
Andrea <tiillin@yahoo.com>
2018-06-02 08:16:18 -0500

    Estilos

--- elbosque_estereo/emisora/static/emisora/js/main.js
+++ elbosque_estereo/emisora/static/emisora/js/main.js
@@ -2,7 +2,21 @@
 $(document).ready(function() {
     $("#start").click(function() {
         $('#header').hide();
-        $('#contact').show();
-        $("#play-button").click();
+        $('.program').hide();
+        $('.onAir').show();
+       $("#play-button").click();      
     });
+
+    $("#program").click(function() {
+        $('#header').hide();
+        $('.onAir').hide();
+        $('.program').show();
+    });
+
+    $("#logo").click(function() {
+        $('#header').hide();
+        $('.program').hide();
+        $('.onAir').show();
+    });
+
 });
